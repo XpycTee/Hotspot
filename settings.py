d1e20fa41fcd2f95b3c15a003410715b612451d6
XpycTee <i@xpyctee.ru>
2024-02-01 17:06:58 +0500

    Refactor SMSRU_API_KEY to SMS_API_KEY in settings.py file

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or _default_sql_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SMSRU_API_KEY = os.environ.get('SMSRU_API_KEY')
+    SMS_API_KEY = os.environ.get('SMSRU_API_KEY')
     HOTSPOT_USER = os.environ.get('HOTSPOT_USER') or 'guest'
     HOTSPOT_PASS = os.environ.get('HOTSPOT_PASS') or 'secret'
     COMPANY_NAME = 'ООО "Рога и копыта"'
