2df8f097f43358f9e8444772ef12de2b6e7692bd
XpycTee <i@xpyctee.ru>
2025-04-15 12:53:03 +0500

    Add 'replace' parameter to 'get_translate' function
    Enhance the 'get_translate' function by introducing a 'replace' parameter. This allows specifying a fallback string to return if the translation is not found, instead of defaulting to the original path. Update the logic to use 'replace' when no translation is available.

--- extensions.py
+++ extensions.py
@@ -7,12 +7,15 @@ from settings import Config
 cache = Cache()
 
 
-def get_translate(path, lang=None):
+def get_translate(path, replace=None, lang=None):
     supported_languages = Config.LANGUAGE_CONTENT.keys()
     # Определяем язык: сначала из параметра, затем из сессии, затем из заголовка, и, наконец, по умолчанию
     lang = lang or \
            session.get("user_lang") or \
            request.accept_languages.best_match(supported_languages)
+    
+    if not replace:
+        replace = path
 
     # Проверяем, поддерживается ли язык, иначе используем язык по умолчанию
     if lang not in Config.LANGUAGE_CONTENT:
@@ -22,4 +25,4 @@ def get_translate(path, lang=None):
     translation = jmespath.search(f"{lang}.{path}", Config.LANGUAGE_CONTENT)
 
     # Возвращаем перевод, если он найден, иначе возвращаем исходный путь
-    return translation if isinstance(translation, str) else path
+    return translation if isinstance(translation, str) else replace
