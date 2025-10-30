1bc782ca49b67bb12e3f2f4ac41e38b5f3e34a80
XpycTee <i@xpyctee.ru>
2025-10-30 10:03:53 +0500

    refactor: improve phone number normalization and enhance session validation in auth routes

--- app/pages/auth.py
+++ app/pages/auth.py
@@ -1,14 +1,11 @@
 # Imports
 import datetime
-import json
 import re
 
 from hashlib import md5
 from random import randint
-import ssl
 
 # Importing Blueprint for creating Flask blueprints
-import certifi
 from flask import Blueprint, jsonify
 
 from sqlalchemy import select, update
@@ -40,6 +37,8 @@ auth_bp = Blueprint('auth', __name__)
 
 
 def _octal_string_to_bytes(oct_string):
+    if not oct_string:
+        return b''
     # Split the octal string by backslash and process each part
     byte_nums = []
     for octal_num in oct_string.split("\\")[1:]:
@@ -52,10 +51,21 @@ def _octal_string_to_bytes(oct_string):
     return bytes(byte_nums)
 
 
+def normalize_phone(phone_number: str) -> str:
+    """Normalize phone to digits, leading 7 for Russia-style numbers."""
+    if not phone_number:
+        return ''
+    num = re.sub(r'\D', '', phone_number)
+    num = re.sub(r'^8', '7', num)
+    if num.startswith('07'):  # guard: if weird leading zero
+        num = '7' + num[2:]
+    return num
+
+
 def _check_employee(phone_number):
     """Проверка, является ли номер телефона сотрудником."""
     employees = fetch_employees()  # Используем кешированные данные
-    sova_phone_number = re.sub(r'^(\+?7|8)', '', phone_number)
+    sova_phone_number = re.sub(r'^(\+?7|8)', '', phone_number) if phone_number else ''
     expression = f"employee[].phone[].number | contains([], '{sova_phone_number}')"
     in_base = bool(jmespath.search(expression, employees))
     if not in_base:
@@ -68,14 +78,16 @@ def _check_employee(phone_number):
 @auth_bp.route('/', methods=['POST', 'GET'])
 def index():
     required_keys = ['link-login-only', 'link-orig', 'mac']
-    if not all(key in set(request.form.keys()) for key in required_keys) or \
-        not all(key in set(session.keys()) for key in required_keys):
-            if 'link-orig' not in session.keys():
-                abort(400)
-            else:
-                redirect(session.get('link-orig'), 302)
-    else:
-        return redirect(url_for('auth.login'), 302)
+    has_form = all(key in set(request.form.keys()) for key in required_keys)
+    has_session = all(key in set(session.keys()) for key in required_keys)
+
+    if not has_form and not has_session:
+        if 'link-orig' not in session.keys():
+            abort(400)
+        else:
+            redirect(session.get('link-orig'), 302)
+
+    return redirect(url_for('auth.login'), 302)
 
 
 @auth_bp.route('/sendin', methods=['POST', 'GET'])
@@ -108,8 +120,8 @@ def sendin():
     session.clear()
     session['link-orig'] = link_orig
 
-    db_phone = ClientsNumber.query.filter_by(phone_number=phone_number).first()
     now_time = datetime.datetime.now()
+    db_phone = _get_or_create_client(phone_number, now_time)
     # Обновляем поле last_seen, если запись уже существует
     try:
         db_phone.last_seen = now_time
@@ -118,18 +130,14 @@ def sendin():
     except IntegrityError:
         db.session.rollback()
         current_app.logger.error("Failed to update last_seen for phone number: %s", phone_number)
-    return f"""
-    <html>
-        <body onload="document.forms[0].submit()">
-            <form action="{link_login_only}" method="post">
-                <input type="hidden" name="username" value="{username}">
-                <input type="hidden" name="password" value="{password}">
-                <input type="hidden" name="dst" value="{link_orig}">
-                <input type="hidden" name="popup" value="true">
-            </form>
-        </body>
-    </html>
-    """
+    
+    return render_template(
+        'auth/sendin.html', 
+        link_login_only=link_login_only, 
+        username=username,
+        password=password,
+        link_orig=link_orig
+    )
 
 
 @auth_bp.route('/test_login', methods=['GET'])
@@ -137,8 +145,8 @@ def test_login():
     if not current_app.debug:
         abort(404)
     required_keys = ['link-login-only', 'link-orig', 'mac']
-    result = [key in set(request.values.keys()) for key in required_keys]
-    if not all(result):
+    has_requirements = all(key in set(request.values.keys()) for key in required_keys)
+    if not has_requirements:
         abort(400)
     else:
         [session.update({k: v}) for k, v in request.values.items()]
@@ -151,12 +159,13 @@ def login():
     error = session.pop('error', None)
 
     required_keys = ['link-login-only', 'link-orig', 'mac']
+    has_form = all(key in set(request.form.keys()) for key in required_keys)
+    has_session = all(key in set(session.keys()) for key in required_keys)
 
     current_app.logger.debug(f'Session data before form: {[item for item in session.items()]}')
 
-    if not all(key in set(request.form.keys()) for key in required_keys):
-        if not all(key in set(session.keys()) for key in required_keys):
-            abort(400)
+    if not has_form and not has_session:
+        abort(400)
     else:
         [session.update({k: v}) for k, v in request.values.items()]
 
@@ -188,8 +197,7 @@ def code():
     phone_number = request.form.get('phone')
 
     if phone_number:
-        phone_number = re.sub(r'^(\+?7|8)', '7', phone_number)
-        phone_number = re.sub(r'\D', '', phone_number)
+        phone_number = normalize_phone(phone_number)
 
         if Blacklist.query.filter_by(phone_number=phone_number).first():
             abort(403)
@@ -288,7 +296,7 @@ def _get_or_create_client(phone_number, now_time):
     return db_phone
 
 
-def _create_wifi_client_if_not_exitst(mac, now_time, is_employee, db_phone):
+def _create_wifi_client_if_not_exists(mac, now_time, is_employee, db_phone):
     """Создать запись WiFi клиента по MAC-адресу, если нету."""
     db_client = db.session.execute(
         select(WifiClient).where(WifiClient.mac == mac).with_for_update()
@@ -322,6 +330,9 @@ def _update_expiration(mac, delay):
 def auth():
     mac = session.get('mac')
     phone_number = session.get('phone')
+    if not mac or not phone_number:
+        abort(400)
+    
     form_code = request.form.get('code')
     user_code = cache.get(f'code_{phone_number}')
 
@@ -342,7 +353,7 @@ def auth():
         is_employee = _check_employee(phone_number)
 
         # Получение или создание записи WiFi клиента
-        _create_wifi_client_if_not_exitst(mac, now_time, is_employee, db_phone)
+        _create_wifi_client_if_not_exists(mac, now_time, is_employee, db_phone)
 
         # Обновление времени истечения
         users_config = current_app.config['HOTSPOT_USERS']
