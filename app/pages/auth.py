b59f47f4e72ff1bd121665d17cea4ee550c3ea88
XpycTee <i@xpyctee.ru>
2025-10-30 09:21:46 +0500

    fix: request validation to use 'all' instead of 'any' for required keys in index, test_login, and login routes

--- app/pages/auth.py
+++ app/pages/auth.py
@@ -68,8 +68,8 @@ def _check_employee(phone_number):
 @auth_bp.route('/', methods=['POST', 'GET'])
 def index():
     required_keys = ['link-login-only', 'link-orig', 'mac']
-    if not any(key in set(request.form.keys()) for key in required_keys) or \
-        not any(key in set(session.keys()) for key in required_keys):
+    if not all(key in set(request.form.keys()) for key in required_keys) or \
+        not all(key in set(session.keys()) for key in required_keys):
             if 'link-orig' not in session.keys():
                 abort(400)
             else:
@@ -136,9 +136,9 @@ def sendin():
 def test_login():
     if not current_app.debug:
         abort(404)
-    required_keys = ['chap-id', 'chap-challenge', 'link-login-only', 'link-orig', 'mac']
+    required_keys = ['link-login-only', 'link-orig', 'mac']
     result = [key in set(request.values.keys()) for key in required_keys]
-    if not any(result):
+    if not all(result):
         abort(400)
     else:
         [session.update({k: v}) for k, v in request.values.items()]
@@ -154,8 +154,8 @@ def login():
 
     current_app.logger.debug(f'Session data before form: {[item for item in session.items()]}')
 
-    if not any(key in set(request.form.keys()) for key in required_keys):
-        if not any(key in set(session.keys()) for key in required_keys):
+    if not all(key in set(request.form.keys()) for key in required_keys):
+        if not all(key in set(session.keys()) for key in required_keys):
             abort(400)
     else:
         [session.update({k: v}) for k, v in request.values.items()]
