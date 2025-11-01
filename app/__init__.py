fe9cac987e826b3f30e29371206bf1089a687152
XpycTee <i@xpyctee.ru>
2025-11-01 13:53:22 +0500

    revert logging

--- app/__init__.py
+++ app/__init__.py
@@ -79,8 +79,8 @@ def create_app(config_class=Config):
     if init:
         app = Flask(__name__)
 
-        configure_logging(app)
         config_class.init_app(app)
+        configure_logging(app)
         
         app.json = CustomJSONProvider(app)
 
