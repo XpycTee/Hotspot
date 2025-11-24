646ba4f11cbf753767eb223556a928f5403293a6
XpycTee <i@xpyctee.ru>
2025-11-24 16:36:21 +0500

    feat: remove old database models and implement SMS sender classes for various gateways

--- radius/server.py
+++ radius/server.py
@@ -1,33 +1,12 @@
 import logging
-import os
-from pyrad2 import dictionary, server
+
+from pyrad2 import server
 from pyrad2.constants import PacketType
 
-from core.db.session import create_all
 from core.wifi.auth import authenticate_by_mac
 
-logging.basicConfig(level=logging.INFO)
 
-TEST_SERVER = os.environ.get("TEST_RADIUS_SERVER")
-TEST_SECRET = os.environ.get("TEST_RADIUS_SECRET")
-
-RADIUS_SERVER = TEST_SERVER
-RADIUS_SECRET = TEST_SECRET.encode()
-
-
-def auth_by_mac(mac, reply):
-    logging.debug(f"Find {mac} in db")
-    client = authenticate_by_mac(mac)
-    status = client.get('status')
-    if status == "OK":
-        employee = client.get("employee")
-        reply.AddAttribute("MT-Group", "employee" if employee else "guest")
-        reply.code = PacketType.AccessAccept
-    elif status == ["NOT_FOUND", "EXPIRED", "BLOCKED"]:
-        reply.code = PacketType.AccessReject
-
-
-class TESTRADIUS(server.Server):
+class HotspotRADIUS(server.Server):
     def HandleAuthPacket(self, pkt):
         logging.info("Received an authentication request")
         logging.debug("Attributes: ")
@@ -42,7 +21,12 @@ class TESTRADIUS(server.Server):
             mac = pkt.get("Calling-Station-Id", [""])[0]
 
         if mac:
-            auth_by_mac(mac, reply)
+            client = authenticate_by_mac(mac)
+            status = client.get('status')
+            if status == "OK":
+                employee = client.get("employee")
+                reply.AddAttribute("MT-Group", "employee" if employee else "guest")
+                reply.code = PacketType.AccessAccept
             
         reply.add_message_authenticator()
         self.SendReplyPacket(pkt.fd, reply)
@@ -58,15 +42,3 @@ class TESTRADIUS(server.Server):
         reply.code = 45
         self.SendReplyPacket(pkt.fd, reply)
 
-
-if __name__ == "__main__":
-    create_all()
-
-    srv = TESTRADIUS(dict=dictionary.Dictionary("dictionary"), coa_enabled=True)
-
-    srv.hosts[RADIUS_SERVER] = server.RemoteHost(
-        RADIUS_SERVER, RADIUS_SECRET, "cap-test"
-    )
-    srv.BindToAddress("0.0.0.0")
-
-    srv.Run()
