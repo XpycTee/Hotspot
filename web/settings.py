88e8fc7239e83b129f9751fa73865f275122c858
XpycTee <i@xpyctee.ru>
2025-12-02 14:08:21 +0500

    refactor(config): remove admin settings and simplify env prefixes
    The update removes unused admin-related configuration, including defaults and the configure_admin method, to streamline the settings module. It also consolidates environment variable handling by replacing nested HOTSPOT_ and WEB_ prefixes with a single HOTSPOT_WEB_ prefix, reducing complexity and improving clarity.

--- web/settings.py
+++ web/settings.py
@@ -1,4 +1,3 @@
-import json
 import logging
 import os
 from environs import Env
@@ -14,15 +13,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    LANGUAGE_FOLDER = "web/static/language"
-    DEFAULT_ADMIN_USERNAME = 'admin'
-    DEFAULT_ADMIN_PASSWORD = 'admin'
-    DEFAULT_ADMIN_MAX_LOGIN_ATTEMPTS = 3
-    DEFAULT_ADMIN_LOCKOUT_TIME = 5
     DEFAULT_COMPANY_NAME = 'Default Company'
 
-    # Переменные класса для хранения конфигурации
-    ADMIN = None
     SECRET_KEY = None
     COMPANY_NAME = None
     DEBUG = None
@@ -37,25 +29,7 @@ class Config:
         with env.prefixed("FLASK_"):
             cls.SECRET_KEY = env.str('SECRET_KEY', None)
 
-        with env.prefixed("HOTSPOT_"):
-            cls.ADMIN = cls.configure_admin()
-            with env.prefixed("WEB_"):
-                cls.COMPANY_NAME = os.environ.get('COMPANY_NAME', cls.DEFAULT_COMPANY_NAME)
+        with env.prefixed("HOTSPOT_WEB_"):
+            cls.COMPANY_NAME = os.environ.get('COMPANY_NAME', cls.DEFAULT_COMPANY_NAME)
 
         app.config.from_object(cls)
-
-    @classmethod
-    def configure_admin(cls):
-        with env.prefixed("ADMIN_"):
-            username = env.str('USERNAME', cls.DEFAULT_ADMIN_USERNAME)
-            password = env.str('PASSWORD', cls.DEFAULT_ADMIN_PASSWORD)
-            max_login_attempts = env.int('MAX_LOGIN_ATTEMPTS', cls.DEFAULT_ADMIN_MAX_LOGIN_ATTEMPTS)
-            lockout_time = env.int('LOCKOUT_TIME', cls.DEFAULT_ADMIN_LOCKOUT_TIME)
-        
-        password_hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-        return {
-            'username': username, 
-            'password': password_hashed, 
-            'max_login_attempts': max_login_attempts, 
-            'lockout_time': lockout_time
-        }
