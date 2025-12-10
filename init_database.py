8ebf554bdbaabdc33ccfaba664b67f208d41975d
XpycTee <i@xpyctee.ru>
2025-12-10 16:39:44 +0500

    feat: initialize wifi clients as offline on database startup
    Add sqlalchemy update import and get_session function to initialize all wifi client records with online=False when the database is created. This ensures a clean state where all clients start as offline until they reconnect.

--- init_database.py
+++ init_database.py
@@ -1,5 +1,6 @@
+from sqlalchemy import update
 from core.database.models import Model
-from core.database.session import engine
+from core.database.session import engine, get_session
 
 import core.database.models.employee
 import core.database.models.employee_phone
@@ -9,3 +10,8 @@ import core.database.models.blacklist
 
 Model.metadata.create_all(engine)
 
+
+with get_session() as db_session:
+    query = update(core.database.models.wifi_client.WifiClient).values(online=False)
+    db_session.execute(query)
+    db_session.commit()
